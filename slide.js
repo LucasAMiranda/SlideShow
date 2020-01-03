cbe8edb981e2272c98ca3953110b24070e6e59ba
Lucas Alencar Miranda <lucasa.miranda@hotmail.com>
2020-01-03 16:48:01 -0300

    Slide-Show

---
+++ slide.js
@@ -0,0 +1,20 @@
+/**
+ * Js - Miniprojeto Slide Show
+ * @author Lucas Miranda */ 
+
+ var intervalo = 3000;
+
+ function slide1(){
+     document.getElementById("banner").src="banner1.jpg";
+     setTimeout("slide2()",intervalo);
+ }
+
+ function slide2(){
+    document.getElementById("banner").src="banner2.jpg";
+    setTimeout("slide3()",intervalo);
+}
+
+function slide3(){
+    document.getElementById("banner").src="banner3.jpg";
+    setTimeout("slide1()",intervalo);
+}
